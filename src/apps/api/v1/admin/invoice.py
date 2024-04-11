b89a1e275371b34a0f7c700f7902aff0a9be7cc0
Hamze <hamzezn@gmail.com>
2024-04-11 16:40:18 +0330

    :bug:

--- src/apps/api/v1/admin/invoice.py
+++ src/apps/api/v1/admin/invoice.py
@@ -52,7 +52,7 @@ async def create_new_invoice(
     payload: invoice_schema.InvoiceCreateIn,
     _: User = Security(get_current_user, scopes=[entity, "create"]),
 ):
-    store = store_crud.get_object(criteria={"_id": payload.store_id})
+    store = await store_crud.get_object(criteria={"_id": payload.store_id})
     customer = users_crud.get_object(criteria={"_id": payload.customer_id})
     result_data = await invoice_controller.create_new_obj(
         new_data=payload, customer=customer, store=store
